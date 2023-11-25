e8de151c6bfa1c1f749d13f06658ac301bad9f0d
DanielJedrasik <djedrasik3@gmail.com>
2023-11-25 22:01:10 +0100

    added transactions + style changes

---
+++ src/components/TransactionHistory/TransactionHistoryItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const TransactionHistoryItem = ({ id, type, amount, currency }) => {
+  return (
+    <tr id={id}>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
+  );
+};
+TransactionHistoryItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
+export default TransactionHistoryItem;
